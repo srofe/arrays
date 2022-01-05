86c08d6334f39c924b9628e403e5d93b7d2add74
Simon Rofe <simon.rofe@ieee.org>
2022-01-06 08:50:21 +1000

    Create method to sum a number of array slices

--- sum_test.go
+++ sum_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("The Sum function shall add all elements of an array", func(t *testing.T) {
@@ -9,6 +12,16 @@ func TestSum(t *testing.T) {
 		want := 15
 		if got != want {
 			t.Errorf("got %d want %d given: %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("The SumAll function shall sum a varying number of array slices and return a slice with the totals for arch slice", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
 		}
 	})
 }
