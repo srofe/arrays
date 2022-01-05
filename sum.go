4e470f359d61b008a7dd5f1aec7294280bc67eb6
Simon Rofe <simon.rofe@ieee.org>
2022-01-06 08:57:41 +1000

    Create a SumAllTails function
    This sums the tails of each array slice. The tail is the array slice
    with the first item removed.

--- sum.go
+++ sum.go
@@ -14,3 +14,10 @@ func SumAll(numbersToSum ...[]int) (sums []int) {
 	}
 	return
 }
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers[1:]))
+	}
+	return
+}
--- sum_test.go
+++ sum_test.go
@@ -25,3 +25,13 @@ func TestSumAll(t *testing.T) {
 		}
 	})
 }
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("The SumAllTails function shall sum the tail of each array slice passed", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
