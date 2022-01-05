c71b373033f4f669d58f1880d398036a8a533ddd
Simon Rofe <simon.rofe@ieee.org>
2022-01-06 07:17:25 +1000

    Remove second test as it is now redundant

--- sum_test.go
+++ sum_test.go
@@ -11,13 +11,4 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given: %v", got, want, numbers)
 		}
 	})
-
-	t.Run("The Sum function shall add collections of any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
-		got := Sum(numbers)
-		want := 6
-		if got != want {
-			t.Errorf("got %d want %d given: %v", got, want, numbers)
-		}
-	})
 }
