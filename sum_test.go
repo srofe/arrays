ca7f6c04c8ad5e74a3d60e716570259149500551
Simon Rofe <simon.rofe@ieee.org>
2022-01-06 07:10:50 +1000

    Embed test in a sub test

--- sum_test.go
+++ sum_test.go
@@ -3,10 +3,12 @@ package arrays
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
-	if got != want {
-		t.Errorf("got %d want %d given: %v", got, want, numbers)
-	}
+	t.Run("The Sum function shall add all elements of an array.", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+		if got != want {
+			t.Errorf("got %d want %d given: %v", got, want, numbers)
+		}
+	})
 }
